97abb5af71ea46b82758ef96a38532f13d25cea0
Lucian M <lucian.maran@outlook.com>
2014-04-28 14:28:01 +0300

    Implement Login form (in Angular, with WebAPI as backend)

--- Web/App/controllers/loginController.js
+++ Web/App/controllers/loginController.js
@@ -1,22 +1,38 @@
-﻿app.controller('loginController', function ($scope, $http, $window) {
-    //$scope.user = { email: 'aaa@outlook.com', password: 'foobar' };
-    $scope.user = { email: '', password: '' };
+﻿app.controller('loginController', function ($scope, $rootScope, $http, $window, $location) {
+    $scope.user = { userName: 'test2@outlook.com', password: 'Aa1111@'};
+    //$scope.errors = {};
+
     $scope.message = '';
+
     $scope.submit = function () {
+        $scope.user.grant_type = 'password';
+
+        // the OWIN OAuth provider is expecting the post to the "/Token" service to be form encoded and not json encoded
+        // so, the expected request should be: "userName=test2%40outlook.com&password=Aa1111%40&grant_type=password"
+        // if you want to not depend on JQuery: http://stackoverflow.com/a/14868725
+        var dataAsFormEncoded = $.param($scope.user);
+
         $http
-          .post('/authenticate', $scope.user)
+          .post('/token', dataAsFormEncoded, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
           .success(function (data, status, headers, config) {
-              $window.sessionStorage.token = data.token;
-              $scope.message = 'Welcome';
+
+              $window.localStorage.token = data.access_token;
+              $rootScope.currentToken = $window.localStorage.token;
+
+              $scope.message = JSON.stringify(data, null, 4);
+              //$location.path('/');
           })
           .error(function (data, status, headers, config) {
               // Erase the token if the user fails to log in
-              delete $window.sessionStorage.token;
+              delete $window.localStorage.token;
 
               // Handle login errors here
-              $scope.message = 'Error: Invalid user or password';
+              //$scope.message = 'Error: Invalid user or password';
+              $scope.message = JSON.stringify(data, null, 4);
           });
     };
+    //$rootScope.localStorage = $window.localStorage.token;
+    
 });
 
 
--- Web/App/controllers/navbarController.js
+++ Web/App/controllers/navbarController.js
@@ -1,4 +1,8 @@
-﻿app.controller('navbarController', function ($scope, $location, $translate, $rootScope) {
+﻿app.controller('navbarController', function ($scope, $location, $translate, $rootScope, $window) {
+
+    // Get currentUser from cookie
+    //$rootScope.currentUser = $cookieStore.get('user') || null;
+    $rootScope.currentToken = $window.localStorage.token || null;
 
     $scope.menu = [{
             'title': 'Home',
@@ -6,8 +10,20 @@
         }, {
             'title': 'Admin',
             'link': '/Admin'
-    }];
+        }];
 
+    $scope.logout = function () {
+        //Auth.logout()
+        //.then(function () {
+        //    $location.path('/login');
+        //});
+
+        delete $window.localStorage.token;
+        $rootScope.currentToken = null;
+        $location.path('/Account/Login');
+    };
+
+    // http://stackoverflow.com/a/18562339
     $scope.isActive = function (route) {
         return route === $location.path();
     };
