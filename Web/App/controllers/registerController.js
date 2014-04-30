13612a19bd21798bb6e7a6d47d0dbcf4d270cb9f
Lucian M <lucian.maran@outlook.com>
2014-04-30 21:31:16 +0300

    Add validation for Registration Form

--- Web/App/controllers/registerController.js
+++ Web/App/controllers/registerController.js
@@ -5,34 +5,43 @@
 
     $scope.message = '';
 
-    $scope.submit = function () {
-        authService.register($scope.user)
-            .then(function (data) {
+    $scope.submit = function (userForm) {
 
-                // auto login with new created credentials
-                var userCredentials = { userName: $scope.user.email, password: $scope.user.password };
-                authService.login(userCredentials)
-                    .then(function (data) {
-                        $window.localStorage.token = data.access_token;
-                        $rootScope.currentToken = $window.localStorage.token;
+        $scope.submitted = true;
+        //alert(userForm.$valid);
+        if (userForm.$valid) {
+            authService.register($scope.user)
+                .then(function (data) {
 
-                        $scope.message = JSON.stringify(data, null, 4);
-                        $location.path('/');
-                    })
-                    .catch(function (err) {
-                        delete $window.localStorage.token;
-                        $rootScope.currentToken = null;
+                    // auto login with new created credentials
+                    var userCredentials = { userName: $scope.user.email, password: $scope.user.password };
+                    authService.login(userCredentials)
+                        .then(function (data) {
+                            $window.localStorage.token = data.access_token;
+                            $rootScope.currentToken = $window.localStorage.token;
 
-                        $scope.message = JSON.stringify(err.data, null, 4);
-                    });
+                            $scope.message = JSON.stringify(data, null, 4);
+                            $location.path('/');
+                        })
+                        .catch(function (err) {
+                            delete $window.localStorage.token;
+                            $rootScope.currentToken = null;
 
-                $scope.message = JSON.stringify(data, null, 4);
-                //$location.path('/');
-            })
-            .catch(function (err) {
-                $scope.message = JSON.stringify(err.data, null, 4);
-            });
+                            $scope.message = JSON.stringify(err.data, null, 4);
+                        });
 
+                    $scope.message = JSON.stringify(data, null, 4);
+                    //$location.path('/');
+                })
+                .catch(function (err) {
+                    //$scope.message = JSON.stringify(err.data, null, 4);
+                    alert(JSON.stringify(err.data, null, 4));
+                });
+
+        }
+        else{
+            //alert('Invalid form');
+        }
     };
 
 });
---
+++ Web/App/directives/ngMatch.js
@@ -0,0 +1,17 @@
+﻿app.directive('match', function () {
+    // http://ngmodules.org/modules/angular-input-match
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+        scope: {
+            match: '='
+        },
+        link: function (scope, elem, attrs, ctrl) {
+            scope.$watch(function () {
+                return (ctrl.$pristine && angular.isUndefined(ctrl.$modelValue)) || scope.match === ctrl.$modelValue;
+            }, function (currentValue) {
+                ctrl.$setValidity('match', currentValue);
+            });
+        }
+    };
+});
