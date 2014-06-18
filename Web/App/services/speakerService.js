6eee57882bc3208c14831b50aab09e6c58cc16cb
Lucian M <lucian.maran@outlook.com>
2014-06-18 20:01:31 +0300

    add file speakers.html

---
+++ Web/App/services/speakerService.js
@@ -0,0 +1,19 @@
+﻿app.factory('speakerService', ['$http', function ($http) {
+    var factory = {};
+
+
+    factory.getSpeakers = function (eventId) {
+        return $http.get('/api/' + eventId + '/speakers').then(function (result) {
+            return result.data;
+        });
+    };
+
+    factory.getSpeaker = function (eventId, speakerId) {
+        return $http.get('/api/' + eventId + '/speakers/' + speakerId).then(function (result) {
+            return result.data;
+        });
+    };
+
+    return factory;
+}]);
+
